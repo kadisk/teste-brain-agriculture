10d5478259c091c5fdf92b1c6d22dcba49d762ae
Kaio Cezar <kaio.araujo@tinnova.com.br>
2023-12-14 01:27:16 -0300

    Listagem produtor rural

--- brain-agriculture-front-end/src/app/page.tsx
+++ brain-agriculture-front-end/src/app/page.tsx
@@ -1,10 +1,9 @@
 import {
 	Container,
-	Row,
-	Nav,
-	NavItem,
-	NavLink
+	Row
 } from 'react-bootstrap'
+
+import ProdutorRuralTable from "../componentes/ProdutorRural.table"
 
 export default function Home() {
 	return (
@@ -29,133 +28,7 @@ export default function Home() {
 					<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 						<h1 className="h2">Produtor Rural</h1>
 					</div>
-					<div className="table-responsive small">
-						<table className="table table-striped table-sm">
-							<thead>
-								<tr>
-									<th scope="col">#</th>
-									<th scope="col">Header</th>
-									<th scope="col">Header</th>
-									<th scope="col">Header</th>
-									<th scope="col">Header</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>1,001</td>
-									<td>random</td>
-									<td>data</td>
-									<td>placeholder</td>
-									<td>text</td>
-								</tr>
-								<tr>
-									<td>1,002</td>
-									<td>placeholder</td>
-									<td>irrelevant</td>
-									<td>visual</td>
-									<td>layout</td>
-								</tr>
-								<tr>
-									<td>1,003</td>
-									<td>data</td>
-									<td>rich</td>
-									<td>dashboard</td>
-									<td>tabular</td>
-								</tr>
-								<tr>
-									<td>1,003</td>
-									<td>information</td>
-									<td>placeholder</td>
-									<td>illustrative</td>
-									<td>data</td>
-								</tr>
-								<tr>
-									<td>1,004</td>
-									<td>text</td>
-									<td>random</td>
-									<td>layout</td>
-									<td>dashboard</td>
-								</tr>
-								<tr>
-									<td>1,005</td>
-									<td>dashboard</td>
-									<td>irrelevant</td>
-									<td>text</td>
-									<td>placeholder</td>
-								</tr>
-								<tr>
-									<td>1,006</td>
-									<td>dashboard</td>
-									<td>illustrative</td>
-									<td>rich</td>
-									<td>data</td>
-								</tr>
-								<tr>
-									<td>1,007</td>
-									<td>placeholder</td>
-									<td>tabular</td>
-									<td>information</td>
-									<td>irrelevant</td>
-								</tr>
-								<tr>
-									<td>1,008</td>
-									<td>random</td>
-									<td>data</td>
-									<td>placeholder</td>
-									<td>text</td>
-								</tr>
-								<tr>
-									<td>1,009</td>
-									<td>placeholder</td>
-									<td>irrelevant</td>
-									<td>visual</td>
-									<td>layout</td>
-								</tr>
-								<tr>
-									<td>1,010</td>
-									<td>data</td>
-									<td>rich</td>
-									<td>dashboard</td>
-									<td>tabular</td>
-								</tr>
-								<tr>
-									<td>1,011</td>
-									<td>information</td>
-									<td>placeholder</td>
-									<td>illustrative</td>
-									<td>data</td>
-								</tr>
-								<tr>
-									<td>1,012</td>
-									<td>text</td>
-									<td>placeholder</td>
-									<td>layout</td>
-									<td>dashboard</td>
-								</tr>
-								<tr>
-									<td>1,013</td>
-									<td>dashboard</td>
-									<td>irrelevant</td>
-									<td>text</td>
-									<td>visual</td>
-								</tr>
-								<tr>
-									<td>1,014</td>
-									<td>dashboard</td>
-									<td>illustrative</td>
-									<td>rich</td>
-									<td>data</td>
-								</tr>
-								<tr>
-									<td>1,015</td>
-									<td>random</td>
-									<td>tabular</td>
-									<td>information</td>
-									<td>text</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
+					<ProdutorRuralTable />
 				</main>
 			</Row>
 		</Container>
---
+++ brain-agriculture-front-end/src/componentes/ProdutorRural.table.tsx
@@ -0,0 +1,52 @@
+
+async function getData() {
+    try {
+        const res = await fetch('http://localhost:3333/produtor-rural/list')
+        if (!res.ok) {
+            throw new Error(`Erro ao buscar dados: ${res.status}`)
+        }
+        
+        return  await res.json()
+    } catch (error) {
+        console.error(error)
+        return []
+    }
+}
+
+async function ProdutorRuralTable(props:any) {
+
+    const produtores = await getData()
+
+    return (
+        <div className="table-responsive small">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">CPF</th>
+                        <th scope="col">CNPJ</th>
+                        <th scope="col">Nome do Produtor</th>
+                        <th scope="col">Nome da Fazenda</th>
+                        <th scope="col">Cidade</th>
+                        <th scope="col">Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {produtores && produtores.map((produtor:any) => (
+                        <tr key={produtor.id}>
+                            <td>{produtor.id}</td>
+                            <td>{produtor.cpf}</td>
+                            <td>{produtor.cnpj}</td>
+                            <td>{produtor.nome_produtor}</td>
+                            <td>{produtor.nome_fazenda}</td>
+                            <td>{produtor.cidade}</td>
+                            <td>{produtor.estado}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ProdutorRuralTable
